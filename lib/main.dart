a1f2c099caa7b59260d9cae678a7380717d28c24
Junior <junior.jr.jl@gmail.com>
2023-01-29 21:38:52 -0300

    conclusão do app

--- lib/main.dart
+++ lib/main.dart
@@ -46,38 +46,65 @@ class MyAppState extends ChangeNotifier{
 
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  var selectedIndex = 0;
+
   @override
   Widget build(BuildContext context){
-    return Scaffold(
-      body: Row(
-        children: [
-          SafeArea(
-            child: NavigationRail(
-              extended: false,
-              destinations: [
-                NavigationRailDestination(
-                  icon: Icon(Icons.home), 
-                  label: Text('Home')
-                  ),
-                NavigationRailDestination(
-                  icon: Icon(Icons.favorite), 
-                  label: Text('Favorites')
+
+    Widget page;
+    switch(selectedIndex){
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = FavoritePage();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              SafeArea(
+                child: NavigationRail(
+                  extended: constraints.maxWidth >= 600,
+                  destinations: [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.home), 
+                      label: Text('Home')
+                      ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.favorite), 
+                      label: Text('Favorites')
+                    )
+                  ],
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: (value){
+                    setState(() {
+                      selectedIndex = value;
+                    });
+                  },
                 )
-              ],
-              selectedIndex: 0,
-              onDestinationSelected: (value){
-                print('selected $value');
-              },
-            )
+              ),
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: page,
+                ))
+            ],
           ),
-          Expanded(
-            child: Container(
-              color: Theme.of(context).colorScheme.primaryContainer,
-              child: GeneratorPage(),
-            ))
-        ],
-      ),
+        );
+      }
     );
   }
 }
@@ -145,6 +172,25 @@ class GeneratorPage extends StatelessWidget{
           )
         ],
       ),
+    );
+  }
+}
+
+class FavoritePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    List<Widget> favList = appState.favorites.map((f) => ListTile(leading: Icon(Icons.favorite), title: Text(f.asLowerCase))).toList();
+    if (appState.favorites.isEmpty){
+      return Center(child: Text('No favorites yet'));
+    }
+
+    return ListView(
+      children: [
+        Padding(padding: const EdgeInsets.all(20),
+          child: Text('You have ${appState.favorites.length} favorites:')),
+            ...favList,
+      ],
     );
   }
 }
